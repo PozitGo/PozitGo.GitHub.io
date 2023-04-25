7f151931c6b5ef71f6f9ce075d87b6e4494d8f37
PozitGo <PozitGo.pozitgo2005@gmail.com>
2023-04-25 16:51:50 +0300

    Add Day 4 v.2

--- capitals/capitals.js
+++ capitals/capitals.js
@@ -1,10 +1,9 @@
 fetch("https://restcountries.com/v3.1/region/europe")
   .then((resolve) => resolve.json())
   .then((data) => {
-    let items;
     data.forEach((element) => {
-      items += element.capital[0] + " ";
+      document.getElementById(
+        "Ul-Capitals"
+      ).innerHTML += `<li>${element.capital[0]}`;
     });
-    document.getElementById("Capitals").textContent = items;
   });
-Ы;
